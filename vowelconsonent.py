377fc71c0912300f0217d9b261a295492dc785f3
prathosh01 <47532652+prathosh01@users.noreply.github.com>
2019-08-17 14:47:33 +0530

    Create vowelconsonent.py

---
+++ vowelconsonent.py
@@ -0,0 +1,9 @@
+l=['a','e','i','o','u','A','E','I','O','U']
+c =input("enter the character")
+if c in l:
+    print(" it is vowel")
+elif str(c).isalpha():
+    print("consonent")
+else:
+    print("invalid")
+    
